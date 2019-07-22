9ded339a5c272211b426217cbcd8bd32c157a12a
Fraxcelsior <marten_bonnema@hotmail.com>
2019-07-22 16:24:22 +0200

    Add index and db files

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const Sse = require('json-sse')
+
+const app = express()
+const jsonParser = bodyParser.json()
+const router = new Router()
+const stream = new Sse()
+
+app.use(cors())
+app.use(jsonParser)
+
+
+
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Listening on port: ${port}`))
