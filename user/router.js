011da79438aa684bf58b1ffc3bf34aede3a20d82
AndrewOmajuwa <omajuwaandrew@gmail.com>
2019-07-23 11:08:33 +0200

    Add room factory function to create and get a list of all rooms using the event source

--- user/router.js
+++ user/router.js
@@ -3,5 +3,14 @@ const User = require('./model')
 const router = new Router()
 
 
+//endpoint to create a user
+router.post("/users", (req, res, next) => {
+    const user = {username: req.body.username}
+    User.create(user).then(user => {
+      res.status(201).send({ 
+          Alert: "A new user has been created", 
+          "user": user });
+    });
+  });
 
 module.exports = router
