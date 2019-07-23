fce8d1661c14540c399389ce84238dd0b23dd839
Fraxcelsior <marten_bonnema@hotmail.com>
2019-07-23 11:11:12 +0200

    re add app.use factory

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const Sse = require('json-sse')
+const testFactory = require('./test/router')
 
 const app = express()
 const jsonParser = bodyParser.json()
-const router = new Router()
 const stream = new Sse()
+const testRouter = testFactory(stream)
 
 app.use(cors())
 app.use(jsonParser)
-
+app.use(testRouter)
 
 
 const port = process.env.PORT || 5000
