2a1b0b42fead7c08dfefb80149bc1f90f1386290
MerylGeugies <mgeugies@gmail.com>
2019-09-02 17:09:28 +0200

    Updated README

--- rooms/router.js
+++ rooms/router.js
@@ -1,21 +1,71 @@
-const { Router } = require("express");
-const User = require('../user/model')
-const Card = require('../card/model')
+const {Router} = require("express");
+const User = require('../user/model');
+const Card = require('../card/model');
 const Room = require("./model");
 
 
 module.exports = RoomFactory = stream => {
-  const router = Router();
+    const router = Router();
 
-  router.get("/stream", (req, res, next) => {
-    Room
-      .findAll({ include: [User, Card] })
-      .then(rooms => {
-        const json = JSON.stringify(rooms)
-        stream.updateInit(json)
-        stream.init(req, res)
+    router.get("/stream", (req, res, next) => {
+        Room
+            .findAll({include: [User, Card]})
+            .then(rooms => {
+                const json = JSON.stringify(rooms)
+                stream.updateInit(json)
+                stream.init(req, res)
+            })
+            .catch(err => next(err))
+    })
+
+//finds the user with the right id, updates the retroId from the user. 
+  router.put("/enter-room/:id", (req, res, next) => {
+    const id = parseInt(req.params.id)
+    const user = req.body.user.id
+    User
+      .findByPk(user)
+      .then(user => {
+        user
+          .update({ retroId: id })
+          .then(user => {
+            const updatedUser = user
+            
+//finds all the rooms with the users and cards. Stringify (makes in to one large string) and updates that to the stream
+// and give the response:  updatedUser  
+            Room
+              .findAll({ include: [User, Card] })
+              .then(rooms => JSON.stringify(rooms))
+              .then(rooms => {
+                stream.updateInit(rooms)
+                stream.send(rooms)
+              })
+              .then(() => res.send(updatedUser))
+          })
       })
       .catch(err => next(err))
+  })
+
+  router.put("/enter-room/:id", (req, res, next) => {
+    const id = parseInt(req.params.id)
+    const user = req.body.user.id
+    User
+        .findByPk(user)
+        .then(user => {
+            user
+                .update({retroId: id})
+                .then(user => {
+                    const updatedUser = user
+                    Room
+                        .findAll({include: [User, Card]})
+                        .then(rooms => JSON.stringify(rooms))
+                        .then(rooms => {
+                            stream.updateInit(rooms)
+                            stream.send(rooms)
+                        })
+                        .then(() => res.send(updatedUser))
+                })
+        })
+        .catch(err => next(err))
   })
 
   router.post('/cards', (req, res, next) => {
@@ -23,7 +73,7 @@ module.exports = RoomFactory = stream => {
       .create(req.body)
       .then(card => {
         Room
-          .findAll({ include: [User, Card] })
+          .findAll({include: [User, Card]})
           .then(rooms => JSON.stringify(rooms))
           .then(rooms => {
             stream.updateInit(rooms)
@@ -41,7 +91,7 @@ module.exports = RoomFactory = stream => {
       .create(req.body)
       .then(room => {
         Room
-          .findAll({ include: [User, Card] })
+          .findAll({include: [User, Card]})
           .then(rooms => JSON.stringify(rooms))
           .then(rooms => {
             stream.updateInit(rooms)
@@ -52,34 +102,6 @@ module.exports = RoomFactory = stream => {
       });
   });
 
-//finds the user with the right id, updates the retroId from the user. 
-  router.put("/enter-room/:id", (req, res, next) => {
-    const id = parseInt(req.params.id)
-    const user = req.body.user.id
-    User
-      .findByPk(user)
-      .then(user => {
-        user
-          .update({ retroId: id })
-          .then(user => {
-            const updatedUser = user
-            
-//does Room excitst? Isn't it Retro?
-//finds all the rooms with the users and cards. Stringify (makes in to one large string) and updates that to the stream
-// and give the response:  updatedUser  
-            Room
-              .findAll({ include: [User, Card] })
-              .then(rooms => JSON.stringify(rooms))
-              .then(rooms => {
-                stream.updateInit(rooms)
-                stream.send(rooms)
-              })
-              .then(() => res.send(updatedUser))
-          })
-      })
-      .catch(err => next(err))
-  })
-
   router.put("/room/:id", (req, res, next) => {
     const id = parseInt(req.params.id)
     const user = req.body.user.id
@@ -87,66 +109,82 @@ module.exports = RoomFactory = stream => {
       .findByPk(user)
       .then(user => {
         user
-          .update({ done: true })
+          .update({done: true})
           .then(user => {
             Room
-              .findByPk(id, { include: [User] })
+              .findByPk(id, {include: [User]})
               .then(room => {
                 const checkDone = room.users.every(user => {
                   return user.done === true
                 })
-                room
-                  .update({ done: checkDone })
-                  .then(() => {
-                    Room
-                      .findAll({ include: [User, Card] })
-                      .then(rooms => JSON.stringify(rooms))
-                      .then(rooms => {
-                        stream.updateInit(rooms)
-                        stream.send(rooms)
+                  room
+                    .update({done: checkDone})
+                    .then(() => {
+                       Room
+                        .findAll({include: [User, Card]})
+                        .then(rooms => JSON.stringify(rooms))
+                        .then(rooms => {
+                          stream.updateInit(rooms)
+                          stream.send(rooms)
+                        })
                       })
-                  })
-              })
-              .then(() => {
-                res.status(201).json(user)
-              })
-          })
-      })
-      .catch(err => next(err))
+                })
+                .then(() => {
+                  res.status(201).json(user)
+                })
+            })
+        })
+        .catch(err => next(err))
   })
 
   router.put("/reset/:id", (req, res, next) => {
     const id = parseInt(req.params.id)
     const user = req.body.user.id
-    User
-      .findByPk(user)
-      .then(user => {
-        user
-          .update({ done: false })
-          .then(user => {
-            Room
-              .findByPk(id, { include: [User] })
-              .then(room => {
-                
-                room
-                  .update({ done: false })
-                  .then(() => {
-                    Room
-                      .findAll({ include: [User, Card] })
-                      .then(rooms => JSON.stringify(rooms))
-                      .then(rooms => {
-                        stream.updateInit(rooms)
-                        stream.send(rooms)
+      User
+        .findByPk(user)
+        .then(user => {
+          user
+            .update({done: false})
+            .then(user => {
+              Room
+                .findByPk(id, {include: [User]})
+                .then(room => {
+
+                  room
+                    .update({done: false})
+                    .then(() => {
+                      Room
+                        .findAll({include: [User, Card]})
+                        .then(rooms => JSON.stringify(rooms))
+                        .then(rooms => {
+                          stream.updateInit(rooms)
+                          stream.send(rooms)
+                        })
                       })
                   })
-              })
-              .then(() => {
-                res.status(201).json(user)
+                  .then(() => {
+                    res.status(201).json(user)
+                  })
               })
           })
-      })
-      .catch(err => next(err))
+          .catch(err => next(err))
   })
-  return router;
-}
 
+  router.delete('/room/:id', (req, res, next) => {
+    const id = parseInt(req.params.id)
+    Room.destroy({
+      where: {
+        id: req.params.id,
+      }
+    })
+    .then(numDeleted => {
+      if (numDeleted) {
+        return res.status(204).end()
+      }
+        return res.status(404).end()
+      })
+    .catch(next)
+  })
+
+    return router;
+}
