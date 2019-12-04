20f43a24419f4ed6e4a083e2b41e3fb541216e30
LavanyaJay <lavanyajay75@gmail.com>
2019-12-04 16:26:58 +0100

    Adding auth file and user db changes

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
-const Sequelize = require('sequelize')
-const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
-const sequelize = new Sequelize(databaseUrl)
+const Sequelize = require("sequelize");
+const databaseUrl =
+  process.env.DATABASE_URL ||
+  "postgres://postgres:secret@localhost:5432/postgres";
+const sequelize = new Sequelize(databaseUrl);
 
-sequelize   
-  .sync({force: false})
-  .then(() => console.log('Database schema has been updated'))
-  .catch(console.error)
-  
-module.exports = sequelize
+sequelize
+  .sync({ force: false })
+  .then(() => console.log("Database schema has been updated"))
+  .catch(console.error);
+
+module.exports = sequelize;
--- user/router.js
+++ user/router.js
@@ -1,14 +1,17 @@
-const { Router } = require('express')
-const User = require('./model')
-const router = new Router()
-
+const { Router } = require("express");
+const bcrypt = require("bcryptjs");
+const User = require("./model");
+const router = new Router();
 
 router.post("/users", (req, res) => {
-    const user = {username: req.body.username}
-    User
-      .create(user).then(user => {
-      res.status(201).send(user)
-    })
-  })
+  const user = {
+    username: req.body.username,
+    password: bcrypt.hashSync(req.body.password, 10)
+  };
 
-module.exports = router
+  User.create(user).then(user => {
+    res.status(201).send(user);
+  });
+});
+
+module.exports = router;
